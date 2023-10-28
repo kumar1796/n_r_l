a2bf285970a4cf52cfb58e3fb958de52367b6941
dinesh <dk23818@gmail.com>
2023-10-28 20:46:34 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,4 +1,35 @@
-const heading = React.createElement("h1", { id: "title" }, "Heading 1");
+/***
+ *
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev abd Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatble with older version of browser
+ * HTTPS on dev
+ * port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ *
+ *
+ *
+ * Transitive Dependencies
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "title" },
+  "Heading 1 for Parcel"
+);
 
 const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
 
